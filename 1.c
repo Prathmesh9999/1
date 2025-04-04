39f949e2b5146e5b4655d04cd6a0e75a3c668cbe
Prathmesh9999 <prathmeshgholap999@gmail.com>
2025-04-05 00:29:18 +0530

    first

---
+++ 1.c
@@ -0,0 +1,17 @@
+//Accept 10 elements in an array  and display them 9th element to 0th element .
+ 
+#include<stdio.h>
+int main()
+{
+	int arr[10];
+	for(int i=0;i<10;i++)
+	{
+		scanf("%d",&arr[i]);
+	}	
+	
+	for(int i=9;i>0;i--)
+	{
+		printf("%d",arr[i]);
+	}	
+	return 0;
+}
